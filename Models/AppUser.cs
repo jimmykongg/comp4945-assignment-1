bb5ab8187c035ce72822b202438bdc558054ca4b
Henry Tan <henrytann00@gmail.com>
2025-02-07 12:47:46 -0800

    Scaffolding

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace QuizApp.Models;
 
 public partial class AppUser
 {
-    public int Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
+    
+    public DateTime? DateOfBirth { get; set; } = DateTime.Today;
+    
+    public byte[]? Photo { get; set; }
 
+    [RegularExpression(@"^[a-zA-Z'\s]{1,20}$", 
+        ErrorMessage = "Letters only, between 1 and 20 characters long.")]
     public string Username { get; set; } = null!;
 
     public string Password { get; set; } = null!;
